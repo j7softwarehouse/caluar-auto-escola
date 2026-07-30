5df9a6e201af0803c1affa1b57d30d7d87c31504
J7 Software House <ramon.p.paula@gmail.com>
2026-07-30 18:45:52 -0300

    feat: Secao sobre conforme referencia

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,7 @@ import Ticker from './components/sections/Ticker'
 import StatsBar from './components/sections/StatsBar'
 import HighlightsCarousel from './components/sections/HighlightsCarousel'
 import Dores from './components/sections/Dores'
-import About from './components/sections/About'
-import Differentials from './components/sections/Differentials'
+import Sobre from './components/sections/Sobre'
 import Categorias from './components/sections/Categorias'
 import Process from './components/sections/Process'
 import Gallery from './components/sections/Gallery'
@@ -27,8 +26,7 @@ function App() {
       <StatsBar />
       <HighlightsCarousel />
       <Dores />
-      <About />
-      <Differentials />
+      <Sobre />
       <Categorias />
       <Process />
       <Gallery />
