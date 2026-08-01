ad9200350c3e895fe28d1af29cfa9c05d7201bbb
J7 Software House <ramon.p.paula@gmail.com>
2026-08-01 16:17:58 -0300

    refactor: Remover Tailwind e componentes orfaos

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,37 @@
-import { AdminProvider } from './context/AdminContext'
 import Navbar from './components/layout/Navbar'
 import Hero from './components/sections/Hero'
 import Ticker from './components/sections/Ticker'
 import StatsBar from './components/sections/StatsBar'
 import Dores from './components/sections/Dores'
-import Sobre from './components/sections/Sobre'
 import Categorias from './components/sections/Categorias'
+import Sobre from './components/sections/Sobre'
 import Processo from './components/sections/Processo'
-import Galeria from './components/sections/Galeria'
 import Avaliacoes from './components/sections/Avaliacoes'
+import Galeria from './components/sections/Galeria'
 import Faq from './components/sections/Faq'
 import Contato from './components/sections/Contato'
 import Mapa from './components/sections/Mapa'
 import Footer from './components/layout/Footer'
-import AdminToggle from './components/admin/AdminToggle'
-import PromoPopup from './components/ui/PromoPopup'
 import WhatsAppFloat from './components/ui/WhatsAppFloat'
 
-function App() {
+export default function App() {
   return (
-    <AdminProvider>
+    <>
       <Navbar />
       <Hero />
       <Ticker />
       <StatsBar />
       <Dores />
-      <Sobre />
       <Categorias />
+      <Sobre />
       <Processo />
-      <Galeria />
       <Avaliacoes />
+      <Galeria />
       <Faq />
       <Contato />
       <Mapa />
       <Footer />
-      <AdminToggle />
-      <PromoPopup />
       <WhatsAppFloat />
-    </AdminProvider>
+    </>
   )
 }
-
-export default App
