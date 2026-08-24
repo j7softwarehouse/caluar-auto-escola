c3d451f1fbb18076bb438bbf3954042d811bc83e
J7 Software House <ramon.p.paula@gmail.com>
2026-08-24 17:04:15 -0300

    fix: remove numero e seta dos cards da galeria
    Cliente pediu para deixar somente as fotos, sem o numero (01, 02...) e
    a setinha de "abrir" que ficavam numa barra abaixo de cada imagem.

--- src/components/sections/Galeria.tsx
+++ src/components/sections/Galeria.tsx
@@ -65,24 +65,6 @@ export default function Galeria() {
                 <div className="gs-img">
                   <img src={foto.src} alt="Aluno aprovado na Caluar Auto-Escola" loading="lazy" />
                 </div>
-                <div className="gs-cap">
-                  <span className="gs-num">{String(i + 1).padStart(2, '0')}</span>
-                  <span className="gs-arrow">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
-                      />
-                    </svg>
-                  </span>
-                </div>
               </div>
             ))}
           </div>
